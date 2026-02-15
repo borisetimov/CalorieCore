ddf454c5b7700a751f0253f5d8de5928960d6ba4
etimo <etimo@BORIS-PC>
2026-02-15 17:37:37 +0200

    Fix Migrations

--- CalorieCore.Web/Program.cs
+++ CalorieCore.Web/Program.cs
@@ -15,7 +15,9 @@ namespace CalorieCore.Web
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+                    x => x.MigrationsAssembly("CalorieCore.Data")
+            ));
 
             builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
             {
